0f13e406151c5807b23e5496a5b9d64eb2284982
Arwin Thoriq R <79559776+arwinthoriq@users.noreply.github.com>
2023-02-15 11:00:01 +0100

    Update login.js
    kalo ada 5 akun, copy 5 script dan di kali 2 waktunya per akun 1000 * 34400

--- login.js
+++ login.js
@@ -10,9 +10,11 @@
 
 (function() {
     'use strict';
+setTimeout(function() {
 var name = ""
 var pass = ""
 document.getElementById('username').value = name
 document.getElementById('password').value = pass
  document.getElementsByTagName('form')[0].submit();
+}, 1000 * 1);
 })();
