429abcefa61021a13efd6292dc427a2454a1a56b
Arwin Thoriq R <79559776+arwinthoriq@users.noreply.github.com>
2023-02-15 10:47:56 +0100

    Create login.js

---
+++ login.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         login
+// @namespace    login
+// @version      1
+// @description  auto login
+// @author       arwinthoriq
+// @match        https://www.youlikehits.com/login.php
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+var name = ""
+var pass = ""
+document.getElementById('username').value = name
+document.getElementById('password').value = pass
+ document.getElementsByTagName('form')[0].submit();
+})();
